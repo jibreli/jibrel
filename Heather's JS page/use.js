cbca6897f3f4e1fc8ba5bd5221e0495bb0e28e4e
Heather Santos <heathsan@uw.edu>
2022-06-03 00:33:30 -0700

    Functions

---
+++ Heather's JS page/use.js
@@ -0,0 +1,21 @@
+let guessMyFavoriteHero = document.getElementById(guessMyHero)
+let guessMyFavoriteVillain = document.getElementById(guessMyVillain)
+let guessMyFavoriteAnti = document.getElementById(guessMyAnti)
+
+guessMyFavoriteHero.addEventListener('click', function () {
+    <button type="button"
+onclick="document.ElementbyId('guessMyHero').innerHtml = Hero()">
+Click To Know My Favorite Hero</button>
+}
+
+guessMyFavoriteVillain.addEventListener('click', function () {
+    <button type="button"
+onclick="document.ElementbyId('guessMyVillain').innerHtml = Hero()">
+Click To Know My Favorite Hero</button>
+}
+
+guessMyFavoriteAnti.addEventListener('click', function () {
+    <button type="button"
+onclick="document.ElementbyId('guessMyAnti').innerHtml = Hero()">
+Click To Know My Favorite Hero</button>
+}
